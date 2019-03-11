f4bc82d0984b89fbbe6d1d03e8c2ee2e838379cf
Brandon Bisbano <brandonbisbano@brandons-macbook-pro.local>
2019-03-11 12:16:38 -0400

    7.3 Completed

---
+++ WeatherGift/WeatherGift/PageVC.swift
@@ -0,0 +1,61 @@
+//
+//  PageVCViewController.swift
+//  WeatherGift
+//
+//  Created by Brandon Bisbano on 3/11/19.
+//  Copyright © 2019 Brandon Bisbano. All rights reserved.
+//
+
+import UIKit
+
+class PageVC: UIPageViewController {
+    
+    var currentPage = 0
+    var locationsArray = ["Local City", "Sydney, Australia", "Accra, Ghana", "Uglich, Russia"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        delegate = self
+        dataSource = self
+        
+        setViewControllers([createDetailVC(forPage: 0)], direction: .forward, animated: false, completion: nil)
+    }
+    
+    func createDetailVC(forPage page: Int) -> DetailVC {
+        
+        currentPage = min(max(0, page), locationsArray.count - 1)
+        
+        let detailVC = storyboard!.instantiateViewController(withIdentifier: "DetailVC") as! DetailVC
+        
+        detailVC.locationsArray = locationsArray
+        detailVC.currentPage = currentPage
+        
+        return detailVC
+    }
+    
+}
+
+extension PageVC: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        if let currentViewController = viewController as? DetailVC {
+            if currentViewController.currentPage < locationsArray.count - 1 {
+                return createDetailVC(forPage: currentViewController.currentPage + 1)
+            }
+        }
+        return nil // IF EITHER OF THOSE CONDITIONS DON'T PAN OUT
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        if let currentViewController = viewController as? DetailVC {
+            if currentViewController.currentPage > 0 {
+                return createDetailVC(forPage: currentViewController.currentPage - 1)
+            }
+        }
+        return nil // IF EITHER OF THOSE CONDITIONS DON'T PAN OUT
+    }
+    
+    
+}
