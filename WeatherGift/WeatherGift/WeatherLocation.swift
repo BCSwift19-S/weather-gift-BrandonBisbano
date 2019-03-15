acf241b46e3c36ed2ab6b3f65f9a49e0cad3b022
Brandon Bisbano <brandonbisbano@brandons-macbook-pro.local>
2019-03-15 16:21:46 -0400

    7.12 Completed

---
+++ WeatherGift/WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Brandon Bisbano on 3/15/19.
+//  Copyright © 2019 Brandon Bisbano. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name = ""
+    var coordinates = ""
+}
+
